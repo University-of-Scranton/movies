7b19734b2716d3c0fdff8a17de61a22763551728
Joe Casabona <jcasabona@gmail.com>
2014-11-19 08:49:45 -0500

    Update movie_db.sql
    Some seed data

--- movie_db.sql
+++ movie_db.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.5
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 05, 2014 at 03:13 AM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
+-- Host: 192.168.30.23
+-- Generation Time: Nov 19, 2014 at 02:52 PM
+-- Server version: 5.5.40-0+wheezy1
+-- PHP Version: 5.4.35-0+deb7u2
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -23,94 +23,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `actors`
---
-
-CREATE TABLE `actors` (
-  `aID` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(25) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `bio` blob NOT NULL,
-  `dob` date NOT NULL,
-  `won_oscar` tinyint(4) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`aID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `genre`
---
-
-CREATE TABLE `genre` (
-  `gID` smallint(6) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`gID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `movies`
 --
 
-CREATE TABLE `movies` (
-  `mID` mediumint(9) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `movies` (
+`mID` mediumint(9) NOT NULL,
   `title` varchar(50) NOT NULL,
   `year_released` year(4) NOT NULL,
-  `synopsis` blob NOT NULL,
+  `synopsis` longtext NOT NULL,
   `was_novel` tinyint(4) NOT NULL,
   `studioID` mediumint(9) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`mID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `movie_actors`
+-- Dumping data for table `movies`
 --
 
-CREATE TABLE `movie_actors` (
-  `maID` int(11) NOT NULL AUTO_INCREMENT,
-  `movieID` mediumint(9) NOT NULL,
-  `actorID` mediumint(9) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`maID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+INSERT INTO `movies` (`mID`, `title`, `year_released`, `synopsis`, `was_novel`, `studioID`, `timestamp`) VALUES
+(1, 'Star Wars: A New Hope', 1977, 'Look, I ain''t in this for your revolution, and I''m not in it for you, Princess. I expect to be well paid. I''m in it for the money. A tremor in the Force. The last time I felt it was in the presence of my old master. I want to come with you to Alderaan. There''s nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me. The plans you refer to will soon be back in our hands. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct.', 0, 1, '2014-11-19 13:28:44'),
+(2, 'Men Who Stare at Goats', 2009, 'Ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.', 0, 2, '2014-11-19 13:28:44');
 
 --
--- Table structure for table `movie_genres`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE `movie_genres` (
-  `mgID` int(11) NOT NULL AUTO_INCREMENT,
-  `movieID` mediumint(9) NOT NULL,
-  `genreID` smallint(6) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`mgID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+ ADD PRIMARY KEY (`mID`);
 
 --
--- Table structure for table `studio`
+-- AUTO_INCREMENT for dumped tables
 --
 
-CREATE TABLE `studio` (
-  `sID` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) NOT NULL,
-  `city` varchar(30) NOT NULL,
-  `state` varchar(2) NOT NULL,
-  `zip` varchar(10) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`sID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+MODIFY `mID` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
