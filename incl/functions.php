509615dc37cf6e1d858df228f7ce9d86b3552007
Sam Savoy <ssavoy@gmail.com>
2014-11-28 14:16:41 -0500

    Setup database connection, added initial dummy functions, added debugging code to sample.php`

--- incl/functions.php
+++ incl/functions.php
@@ -1,5 +1,22 @@
 <?php
+require_once('incl/Database.php');
+$host = 'localhost';
+$u = 'sam';
+$p = 'password!';
+$d = 'movies';
 
+global $db;
+$db = new Database($d, $u, $p, $host);
+
+function test_database(){
+	$db = $GLOBALS['db'];
+	$results = $db->query("SELECT * FROM movies LIMIT 2");
+	return $db->resToArray($results);
+}
+
+function result_dumper($resImport){
+	var_dump($resImport);
+}
 
 
 function print_array( $a ) {
--- sample.php
+++ sample.php
@@ -1,20 +1,36 @@
-<?php require_once( 'incl/header.php' ); ?>
+<?php 
+require_once('incl/header.php');
+require_once('incl/functions.php');
+
+?>
 
  <div class="container mtb">
 	 	<div class="row">
 	 	
-	 		<! -- MAIN CONTENAT AREA -->
+	 		<!--MAIN CONTENT AREA-->
 	 		<div class="col-lg-8">
 	 			<p>The main body of the page goes here</p>
-		 		
-			</div><! --/ MAIN CONTENT AREA -->
+	 			<p><b>Sample DB Connection</p></b>
+	 			<?php
+				$resImport = test_database();
+				result_dumper($resImport);
+		 		?>
+		 		<br>
+		 		<b>Testing below this line</b>
+		 		<p>----------------------</p>
+		 		<?php
+		 			echo "Movie 1 Title: " . $resImport[0]["title"];
+		 		?>
+		 		<p>----------------------</p>
+		 		<b>Testing above this line</b>
+			</div><!--/MAIN CONTENT AREA-->
 	 		
 	 		
-	 		<! -- SIDEBAR -->
+	 		<!-- SIDEBAR-->
 	 		<div class="col-lg-4">
 		 		<p>Sidebar (if any) goes here/</p>
-	 		</div><!-- /SIDEBAR -->
-	 	</div><! --/row -->
-	 </div><! --/container -->
+	 		</div><!--/SIDEBAR-->
+	 	</div><!--row-->
+	 </div><!--container -->
 
 <?php require_once( 'incl/footer.php' ); ?>
