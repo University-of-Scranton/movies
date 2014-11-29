b1eedb3e06f10a82afcde482dad6638a1f2027f6
Sam Savoy <ssavoy@gmail.com>
2014-11-28 23:14:16 -0500

    Added "Stars In" section for specific actor page

--- actors.php
+++ actors.php
@@ -19,19 +19,38 @@ $dob = $selected_actor[0]["dob"];
 	 				if($selected_actor[0]["won_oscar"] == "1"){
 	 					echo "<img src='assets/img/oscar_icon.jpg'>";
 	 					echo "</h3>";
-	 					echo $dob . "</p>";
-	 					calculate_age($dob);
+	 					$age = calculate_age($dob);
+	 					echo "<b>Birthdate:</b> " . $dob . " <i>(" . $age .  ")</i></p>";
 	 				}
 	 				if($selected_actor[0]["won_oscar"] == "0"){
 	 					echo "</h3>";
-	 					echo $dob . "</p>";
 	 					$age = calculate_age($dob);
+	 					echo "<b>Birthdate:</b> " . $dob . " <i>(" . $age .  ")</i></p>";
 	 				}
+
 	 			?>
 	 			<div class="actor_bio">
 	 			<p><?php echo $selected_actor[0]["bio"]; ?></p>
 	 			</div>
-
+	 			<h3>Stars In: </h3>
+	 			<?php
+	 			$appears = movies_with_actor($aID);
+	 			$list_size = sizeof($appears);
+	 			?>
+	 			<table>
+	 				<tr class="table_head">
+	 					<th>Movie</th>
+	 					<th>Year</th>
+	 				</tr>
+	 				<?php
+	 					for($i=0;$i<=$list_size;$i++){
+	 						echo "<tr>";
+	 						echo "<td>" . $appears[$i]["title"] . "</td>";
+	 						echo "<td>" . $appears[$i]["year_released"] . "</td>";
+	 						echo "</tr>";
+	 					}
+	 				?>
+	 			</table>
 			</div><!--/MAIN CONTENT AREA-->
 	 		
 	 		
