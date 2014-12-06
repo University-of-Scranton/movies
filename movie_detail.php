050cf257dfd07f6da23f80cee17c04c9cb90b8da
Sam Savoy <ssavoy@gmail.com>
2014-12-06 16:31:27 -0500

    Added movie studio information

--- movie_detail.php
+++ movie_detail.php
@@ -4,6 +4,8 @@ require_once('incl/functions.php');
 $mID = $_GET['mid'];
 $selected_movie = get_movie_info($mID);
 var_dump($selected_movie);
+$studio = studio_makes_movie($mID);
+$studio = $studio[0]["name"];
 ?>
 
  <div class="container mtb">
@@ -14,9 +16,11 @@ var_dump($selected_movie);
 	 			<h1><?php echo $selected_movie[0]["title"] . " (" . $selected_movie[0]["year_released"] . ")"; ?></h1>
 				<p><?php echo $selected_movie[0]["synopsis"]; ?></p>
 				<?php
+
 					if($selected_movie[0]["was_novel"] == "1"){
 						echo "<p>Based on a novel</p>";
 					}
+					echo "<p>Produced by " . $studio . "</p>";
 				?>
 			</div><!--/MAIN CONTENT AREA-->
 	 		
